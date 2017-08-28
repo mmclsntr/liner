80e9ffe763320749f60346b7daf90c5f26a94995
Shintaro Yamasaki <m1m16h20@oit.ac.jp>
2017-08-28 15:17:38 +0900

    Add apps and appmanager directories

---
+++ apps/gpiomanager/__init__.py
@@ -0,0 +1,2 @@
+from apps.node import Node
+
---
+++ apps/node/__init__.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+class Node:
+  config = None
+
+  def __init__(self):
+      pass
+  
+  def read(self) -> Any:
+    pass
+
+  def write(self, value: Any):
+    pass 
