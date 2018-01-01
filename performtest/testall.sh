277ef961299bb2e3699adec4e0d2fccacc6004b1
Shintaro Yamasaki <m1m16h20@oit.ac.jp>
2018-01-01 14:19:53 +0000

    Update test scripts

--- performtest/testall.sh
+++ performtest/testall.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-for i in {1..20} ;
-do
-# Connectors doesn't exist
-  for num in 150 ; 
-  do
-    for interval in 0.5 1.5 1 2 ;
-     do
-      echo "Count: "$i"  Number: "$num"  Interval: "$interval
-      ./test.sh $num $interval 0
-    done
-  done
-done
-
-exit
-
-for i in {1..10} ;
+for i in {1..100} ;
 do
 # Connectors exist
-  for connector in 1 10 20 50 ; 
+  for connector in 1 10 20 30 40 50 ; 
   do
     for interval in 0.5 1.5 1 2 ;
      do
@@ -28,7 +13,7 @@ do
   done
 
 # Connectors doesn't exist
-  for num in 1 10 50 100 200 250 300 ; 
+  for num in 1 10 50 100 150 200 250 ; 
   do
     for interval in 0.5 1.5 1 2 ;
      do
